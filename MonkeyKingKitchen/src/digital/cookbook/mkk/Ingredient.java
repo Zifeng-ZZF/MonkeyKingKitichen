a8fd96e44ef3fa17915ca12ed67016ed658f78d2
User <User@DESKTOP-E6NBM8C>
2018-05-26 12:07:08 +0200

    entity
    javadoc没写完整

--- MonkeyKingKitchen/src/digital/cookbook/mkk/Ingredient.java
+++ MonkeyKingKitchen/src/digital/cookbook/mkk/Ingredient.java
@@ -1,7 +1,52 @@
 package digital.cookbook.mkk;
 
-public class Ingredient {
-	private String name;
-	
+/**
+ * Ingredient
+ * @author Jisheng
+ * @version 1.0 26/5/2018
+ */
 
+public class Ingredient {
+		//ingredient attributes
+		private String name;
+		private	String unit;
+		private	String processMethod;
+		private	double amount;
+				
+		
+		public String getName() {
+				return name;
+		}
+		public void setName(String name) {
+				this.name = name;
+		}
+		public String getUnit() {
+				return unit;
+		}
+		public void setUnit(String unit) {
+				this.unit = unit;
+		}
+		public String getProcessMethod() {
+				return processMethod;
+		}
+		public void setProcessMethod(String processMethod) {
+				this.processMethod = processMethod;
+		}
+		public double getAmount() {
+				return amount;
+		}
+		public void setAmount(double amount) {
+				this.amount = amount;
+		}
+				
+				
+		
+		
+		
+		
+
+		
+		
+		
 }
+
--- MonkeyKingKitchen/src/digital/cookbook/mkk/Recipe.java
+++ MonkeyKingKitchen/src/digital/cookbook/mkk/Recipe.java
@@ -1,5 +1,89 @@
 package digital.cookbook.mkk;
 
+import java.util.ArrayList;
+
+/**
+ * Recipe
+ * @author Jisheng
+ * @version 1.0 26/5/2018
+ */
+
 public class Recipe {
+		//Recipe attributes
+		private int recipeId;
+		private int uid;
+		private	int preparationTime;
+		private	int cookingTime;
+		private	int servings;
+		private	double rate;
+		private	String type;
+		private	String name;
+		private	ArrayList<Ingredient> ingredients;
+		private	ArrayList<String> preparationSetps;
+		
+		
+		public int getRecipeId() {
+			return recipeId;
+		}
+		public void setRecipeId(int recipeId) {
+			this.recipeId = recipeId;
+		}
+		public int getUid() {
+			return uid;
+		}
+		public void setUid(int uid) {
+			this.uid = uid;
+		}
+		public int getPreparationTime() {
+			return preparationTime;
+		}
+		public void setPreparationTime(int preparationTime) {
+			this.preparationTime = preparationTime;
+		}
+		public int getCookingTime() {
+			return cookingTime;
+		}
+		public void setCookingTime(int cookingTime) {
+			this.cookingTime = cookingTime;
+		}
+		public int getServings() {
+			return servings;
+		}
+		public void setServings(int servings) {
+			this.servings = servings;
+		}
+		public double getRate() {
+			return rate;
+		}
+		public void setRate(double rate) {
+			this.rate = rate;
+		}
+		public String getType() {
+			return type;
+		}
+		public void setType(String type) {
+			this.type = type;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public ArrayList<Ingredient> getIngredients() {
+			return ingredients;
+		}
+		public void setIngredients(ArrayList<Ingredient> ingredients) {
+			this.ingredients = ingredients;
+		}
+		public ArrayList<String> getPreparationSetps() {
+			return preparationSetps;
+		}
+		public void setPreparationSetps(ArrayList<String> preparationSetps) {
+			this.preparationSetps = preparationSetps;
+		}
+		
+		
+		
 
 }
