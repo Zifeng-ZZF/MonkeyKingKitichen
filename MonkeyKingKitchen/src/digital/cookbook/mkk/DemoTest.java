f52f772a3a3f1c6035468eefe1e4a62299d813d8
User <User@FongFong>
2018-05-26 13:50:17 +0200

    Test for searching

---
+++ MonkeyKingKitchen/src/digital/cookbook/mkk/DemoTest.java
@@ -0,0 +1,17 @@
+package digital.cookbook.mkk;
+
+/**
+ * @author Zifeng Zhang
+ * The class for testing search method
+ */
+public class DemoTest {
+
+	/**
+	 * Main function of the class
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+}
