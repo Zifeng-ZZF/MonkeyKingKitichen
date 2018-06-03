71aff6564175650cfac07737cc6c9421a2f26d3f
User <User@FongFong>
2018-06-03 18:05:32 +0200

    recipeFetch added

--- MonkeyKingKitchen/src/digital/cookbook/mkk/DBProcessor.java
+++ MonkeyKingKitchen/src/digital/cookbook/mkk/DBProcessor.java
@@ -84,9 +84,96 @@ public class DBProcessor {
 		return recipeList;
 	}
 
+	/**
+	 * Fetch favorite recipes from db
+	 * @param uid
+	 * @return
+	 */
 	public ArrayList<Recipe> fetchFavorite(int uid) {
 		return null;
 	}
 	
+	public void insertUser(User user) {
+		Connection conn = DBUtil.open();
+		
+        //At first, find the maximum id of current db
+        String idSql = "select max(user_id) from usertb;";
+        Statement statement = null;
+        int maxID = 0;
+        try {
+        	statement = conn.createStatement();
+			ResultSet resultSet = statement.executeQuery(idSql);
+			if(resultSet.next())
+				maxID = resultSet.getInt(1);
+			maxID = 0;
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
+        
+        String sql = "insert into usertb(user_id,user_name,user_passwd) values(?,?,?);";
+        try {
+			PreparedStatement pStatement = conn.prepareStatement(sql);
+			pStatement.setInt(1,maxID+1);
+			pStatement.setString(2, user.getName());
+			pStatement.setString(3, user.getPasswd());
+			
+			pStatement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 	
+	/**
+	 * Insert one recipe into recipetb in DB
+	 * @param recipe
+	 */
+	public void insetRecipe(Recipe recipe, int currentUserID) {
+        Connection conn = DBUtil.open();
+        
+        //At first, find the maximum id of current db
+        String idSql = "select max(recipe_id) from recipetb;";
+        Statement statement = null;
+        int maxID = 0;
+        try {
+        	statement = conn.createStatement();
+			ResultSet resultSet = statement.executeQuery(idSql);
+			if(resultSet.next())
+				maxID = resultSet.getInt(1);
+			maxID = 0;
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
+        
+        
+        String sql = "insert into recipetb(recipe_id,recipe_name,recipe_preparation_time,"
+        		+ "recipe_cooking_time,recipe_averate,recipe_servings,user_id,recipe_type,recipe_steps)"
+        		+ " values(?,?,?,?,?,?,?,?,?);";
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, maxID);
+            pstmt.setString(2,recipe.getName());
+            pstmt.setInt(3,recipe.getPreparationTime());
+            pstmt.setInt(4,recipe.getCookingTime());
+            pstmt.setDouble(5, recipe.getRate());
+            pstmt.setInt(6,recipe.getServings());
+            pstmt.setInt(7,currentUserID);
+            pstmt.setString(8, recipe.getType());
+            String steps = "";
+            for (String step : recipe.getPreparationSetps()) {
+				step += "|";
+				steps += step;
+			}
+            pstmt.setString(9, steps);
+            
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DBUtil.close(conn);
+        }
+	}
+	
+	public void updateRecipe() {
+		
+	}
 }
