ca0fa1267fb1f86b420508b073614f3c9e20d347
User <User@FongFong>
2018-06-05 22:05:18 +0200

    Refine login method

--- MonkeyKingKitchen/src/digital/cookbook/mkk/CookBook.java
+++ MonkeyKingKitchen/src/digital/cookbook/mkk/CookBook.java
@@ -8,20 +8,22 @@ import java.sql.*;
 
 /**
  * The cookbook
+ * 
  * @author Zifeng Zhang, Xinyue Shi
  * @version 2.0.1 28/5/2018
  *
  */
 public class CookBook {
-	
+
 	private String cookBookTitle;
-	private Map<Integer,Recipe> recipes;
+	private Map<Integer, Recipe> recipes;
 	private ArrayList<User> users;
 	private DBProcessor dbProcessor = new DBProcessor();
 	private User currentUser;
-	
+
 	/**
 	 * Constructor
+	 * 
 	 * @param title
 	 */
 	public CookBook(String title) {
@@ -29,60 +31,66 @@ public class CookBook {
 		recipes = dbProcessor.fetchRecipe();
 		users = dbProcessor.fetchUserInfo();
 	}
-	
+
 	/**
 	 * Add recipe to the cookBook
+	 * 
 	 * @param recipe
 	 */
 	public void add(Recipe recipe) {
 		this.recipes.put(recipe.getRecipeId(), recipe);
-		dbProcessor.insetRecipe(recipe,currentUser.getUid());
+		dbProcessor.insetRecipe(recipe, currentUser.getUid());
 	}
-	
+
 	/**
 	 * Use name to get the recipe with the same name
+	 * 
 	 * @param name
 	 * @return null if no result, otherwise return the name-matched recipes
 	 */
 	public Recipe getRecipe(String name) {
 		Set recipeIds = recipes.keySet();
 		for (Object recipeIdObj : recipeIds) {
-			int id = (Integer)recipeIdObj;
+			int id = (Integer) recipeIdObj;
 			String recipeName = recipes.get(id).getName();
-			if(recipeName.equals(name)) {
+			if (recipeName.equals(name)) {
 				return recipes.get(id);
 			}
 		}
 		return null;
 	}
-	
+
 	/**
 	 * Use name to get the recipe with the same name
+	 * 
 	 * @param name
 	 * @return null if no result, otherwise return the name-matched recipes
 	 */
 	public Recipe searchByName(String name) {
 		Set recipeIds = recipes.keySet();
 		for (Object recipeIdObj : recipeIds) {
-			int id = (Integer)recipeIdObj;
+			int id = (Integer) recipeIdObj;
 			String recipeName = recipes.get(id).getName();
-			if(recipeName.equals(name)) {
+			if (recipeName.equals(name)) {
 				return recipes.get(id);
 			}
 		}
 		return null;
 	}
-	
+
 	/**
 	 * Get the total recipes of the cook book.
+	 * 
 	 * @return the recipes list
 	 */
-	public Map<Integer, Recipe> recipesList(){
+	public Map<Integer, Recipe> recipesList() {
 		return this.recipes;
 	}
-	
+
 	/**
-	 * Change amount of preparation time, ingredients amount and cooking time according to servings
+	 * Change amount of preparation time, ingredients amount and cooking time
+	 * according to servings
+	 * 
 	 * @param recipeId
 	 * @param Servings
 	 * @return the altered recipe
@@ -95,21 +103,36 @@ public class CookBook {
 		recipe.setPreparationTime(preparationTime);
 		recipe.setCookingTime(cookingTime);
 		for (Object ingredientObj : recipe.getIngredients()) {
-			double amount = servings * (((Ingredient)ingredientObj).getAmount()) / originalServings;
-			((Ingredient)ingredientObj).setAmount(amount);
+			double amount = servings * (((Ingredient) ingredientObj).getAmount()) / originalServings;
+			((Ingredient) ingredientObj).setAmount(amount);
 		}
 		return recipe;
 	}
-	
+
 	public void register(User user) {
 		this.users.add(user);
 		dbProcessor.insertUser(user);
 	}
-	
-	public void login(String username,String passwd) {
+
+	/**
+	 * Via login, initialize the favoriteList and myRecipeList
+	 * @param username
+	 * @param passwd
+	 */
+	public void login(String username, String passwd) {
 		for (User user : users) {
-			if(user.getName().equals(username) && user.getPasswd().equals(passwd))
+			//Initialize current user
+			if (user.getName().equals(username) && user.getPasswd().equals(passwd)) {
 				this.currentUser = user;
+				//Initialize myFavoriteList
+				user.setMyFavoriteList(dbProcessor.fetchFavorite(user.getUid()));
+				//Initialize myRecipeList
+				for (int recipeId : recipes.keySet()) {
+					Recipe recipe = recipes.get(recipeId);
+					if(recipe.getUid() == user.getUid())
+						user.getMyRecipeList().add(recipe);
+				}
+			}
 		}
 	}
 }
