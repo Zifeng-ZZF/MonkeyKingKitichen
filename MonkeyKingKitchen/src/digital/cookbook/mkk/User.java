7e0023e37ee360fd306c4f9dd6eca0ab9f4c9b35
User <User@FongFong>
2018-06-08 15:29:54 +0200

    add deleteFavorite Method / refine addFavorite Method

--- MonkeyKingKitchen/src/digital/cookbook/mkk/User.java
+++ MonkeyKingKitchen/src/digital/cookbook/mkk/User.java
@@ -8,11 +8,11 @@ import java.util.ArrayList;
  * The entity class of User
  * 
  * @author Jisheng, Zifeng Zhang
- * @version 1.0 26/5/2018 
+ * @version 1.0 26/5/2018
  * @version 2.0 27/5/2018
  */
 
-public class User implements Serializable{
+public class User implements Serializable {
 	// User attributes
 	private int uid;
 	private String name;
@@ -20,9 +20,10 @@ public class User implements Serializable{
 	private ArrayList<Recipe> myFavoriteList;
 	private ArrayList<Recipe> myRecipeList;
 	private DBProcessor dbProcessor = new DBProcessor();
-	
+
 	/**
 	 * Constructor of user
+	 * 
 	 * @param username
 	 * @param passwd
 	 */
@@ -31,36 +32,67 @@ public class User implements Serializable{
 		this.name = username;
 		this.passwd = passwd;
 	}
-	
+
 	/**
 	 * Add recipe to favorite
+	 * 
 	 * @param recipe
 	 */
 	public void addToFavorite(Recipe recipe) {
-		this.myFavoriteList.add(recipe);
-		dbProcessor.insertIntoFavorite(recipe, this.uid);
+		ArrayList<Recipe> iterateList = new ArrayList<>(this.myFavoriteList);
+		int passNum = 0;
+		if (!this.myFavoriteList.isEmpty()) {
+			for (Recipe existRecipe : myFavoriteList) {
+				if((recipe.getRecipeId() == existRecipe.getRecipeId()) &&
+						(recipe.getUid() == existRecipe.getUid())) {
+					System.out.println("Recipe already added.");
+				}else {
+					passNum++;
+				}
+			}
+			
+			if(passNum == myFavoriteList.size()) {
+				iterateList.add(recipe);
+				dbProcessor.insertIntoFavorite(recipe, this.uid);
+			}
+			
+		} else {
+			iterateList.add(recipe);
+			dbProcessor.insertIntoFavorite(recipe, this.uid);
+		}
+		this.myFavoriteList = iterateList;
 	}
 
 	/**
+	 * Delete the favorite recipe from myfavoritetb;
+	 * @param recipe
+	 */
+	public void deleteFavorite(Recipe recipe) {
+		dbProcessor.deleteFavoriteRecipe(recipe.getRecipeId(), this.uid);
+	}
+	
+	/**
 	 * Rate the recipe
+	 * 
 	 * @param rate
 	 */
 	public void rate(double rate, Recipe recipe) {
 		dbProcessor.insertRate(recipe, this.uid, rate);
 	}
-	
+
 	/**
 	 * getters and setters
+	 * 
 	 * @return
 	 */
 	public String getPasswd() {
 		return this.passwd;
 	}
-	
+
 	public int getUid() {
 		return uid;
 	}
-	
+
 	public void setUid(int uid) {
 		this.uid = uid;
 	}
