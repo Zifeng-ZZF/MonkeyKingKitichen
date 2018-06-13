622aeed90d9ce8c2032286e459b1d3ba5ed66787
User <User@FongFong>
2018-06-13 13:40:16 +0200

    redundance remove

--- MonkeyKingKitchen/src/digital/cookbook/mkk/ResultView/ResultViewController.java
+++ MonkeyKingKitchen/src/digital/cookbook/mkk/ResultView/ResultViewController.java
@@ -19,7 +19,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuButton;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
