5b3c181299215716633436a8da486a4e3538ce72
FongFong-game <FongFong-game@LAPTOP-NTEG557U.fritz.box>
2018-06-17 01:20:58 +0800

    Edit view refined

---
+++ MonkeyKingKitchen/src/digital/cookbook/mkk/EditPage/EditViewController.java
@@ -0,0 +1,61 @@
+package digital.cookbook.mkk.EditPage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class EditViewController implements Initializable {
+	
+    @FXML
+    private TextArea stepsTxtArea;
+
+    @FXML
+    private TextField prepareTimeTxtField;
+
+    @FXML
+    private ComboBox<?> ingredientComBox;
+
+    @FXML
+    private TextField servingTxtField;
+
+    @FXML
+    private TextField cookTimeTxtField1;
+
+    @FXML
+    private TextField unitTxtField;
+
+    @FXML
+    private Label recipeName;
+
+    @FXML
+    private Button addProcessBtn;
+
+    @FXML
+    private TextField recipeNameTxtField;
+
+    @FXML
+    private TextArea IngredientsTxtArea;
+
+    @FXML
+    private TextField cookTimeTxtField;
+
+    @FXML
+    private TextField amountTxtField;
+
+    @FXML
+    private TextField processTxtField;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
